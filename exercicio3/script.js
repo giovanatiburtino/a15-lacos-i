485e2719e322810f2c3551fbe7cf321db9d6d7a7
giova <giovanaftiburtino@gmail.com>
2022-09-12 17:46:44 -0300

    Exercicio 1, 2, 3 feitos

---
+++ exercicio3/script.js
@@ -0,0 +1,7 @@
+//Crie um array com 5 strings. Faça um programa que percorra este array e imprima as strings em formato de ranking
+const maioresSeleçoes = ["Brasil", "Alemanha", "Itália", "Argentina", "França"]
+
+for(let ranking = 0; ranking < maioresSeleçoes.length; ranking++){
+    console.log(i + 1, maioresSeleçoes[ranking])
+}
+
